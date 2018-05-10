0e8a6721031c50998e7a3fa5b75c3ee5effe57a7
AR8Stefan <stefan.piatkov@gmail.com>
2018-05-10 15:26:57 -0500

    refactoring part 1

--- app.js
+++ app.js
@@ -34,38 +34,28 @@ document.addEventListener('DOMContentLoaded', () => { // This line helps code to
 		}
 	});
 
-	function createLI(text) {
-		const li = document.createElement('li');
-
-		const span = document.createElement('span');
-		// Displays the value(name) typed in input field into the li.
-		span.textContent = text;
-		li.appendChild(span);  
-
-
-		// Create a label to say if confirmed or not..
-		const label = document.createElement('label');
-		label.textContent = 'Confirmed';
-		// Creates a checkbox for confirming
-		const checkbox = document.createElement('input');
-		checkbox.type = 'checkbox';
-		label.appendChild(checkbox);
-		li.appendChild(label);
-
-		// Creates a button to EDIT name in the list.
-		const editButton = document.createElement('button');
-		editButton.textContent = 'Edit';
-		li.appendChild(editButton);
-
-		// Creates a Button to Remove item from the list
-		const removeButton = document.createElement('button');
-		removeButton.textContent = 'Remove';
-		li.appendChild(removeButton);
-		// Using the same name for both buttons produces an error. make sure to have differnt names.
-
-		// Returns 'li' to the handler - because JS functions are undefined by defualt.
-		return li;
-	};
+  function createLI(text) {
+    function createElement(elementName, propertyName, valueName) {
+      const element = document.createElement(elementName);  
+      element[propertyName] = valueName; 
+      return element;
+    }
+    
+    function appendToLI(elementName, propertyName, valueName) {
+      const element = createElement(elementName, propertyName, valueName);     
+      li.appendChild(element); 
+      return element;
+    }
+    
+    const li = document.createElement('li');
+    appendToLI('span', 'textContent', text);     
+    appendToLI('label', 'textContent', 'Confirmed')
+      .appendChild(createElement('input', 'type', 'checkbox'));
+    appendToLI('button', 'textContent', 'Edit');
+    appendToLI('button', 'textContent', 'Remove');
+    // Remember to name buttons same as "else if" statement.
+    return li;
+  }
 
 	form.addEventListener('submit', (e) => {
 		// Prevents Default behaviour of 'Submit' event.
@@ -110,7 +100,6 @@ document.addEventListener('DOMContentLoaded', () => { // This line helps code to
 			} else if (button.textContent === 'Save') {
 				const input = li.firstElementChild;
 				const span = document.createElement('span');
-				// input.type = 'text';
 				span.textContent = input.value;
 				li.insertBefore(span, input);
 				li.removeChild(input);
